c42cdbb35e2211f1bb8f9352c029ba8692938b52
Harun <67702854+hrndgc@users.noreply.github.com>
2021-04-04 18:13:13 +0300

    Update MyFirstPro.java
    New version at Github.com

--- src/myfirstpackage/MyFirstPro.java
+++ src/myfirstpackage/MyFirstPro.java
@@ -5,6 +5,8 @@ public class MyFirstPro {
 	public static void main(String[] args) {
 		System.out.println("version 1");
 		System.out.println("version 2");
+		System.out.println("version 3");
+		System.out.println("version 4 for pull");
 
 	}
 
